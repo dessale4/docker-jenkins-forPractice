8a9c7cc7101af28c1acf81a3a729c08d8af087e9
dessale4 <dessale4@gmail.com>
2020-09-13 19:26:03 -0500

    Controller class included

---
+++ src/main/java/com/desale/dockerJenkinsIntegration/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.desale.dockerJenkinsIntegration.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping
+	public String getData() {
+		return "Data ready";
+	}
+}
